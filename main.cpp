36726b8ca83ab5d13ad7ca08a2d8797714ddb86c
Neil Johnson <mr.neil.j@gmail.com>
2017-02-03 00:26:08 -0800

    Added first struct to file Completed rest of the structs required

--- main.cpp
+++ main.cpp
@@ -9,5 +9,41 @@ struct CostInfo
     double  food,           // Food costs
             medical,        // Medical costs
             license,        // License fee
-            misc;           // Miscellaneious costs
+            misc;           // Miscellaneous costs
 };
+
+struct PetInfo
+{
+    string  name;           // Pet name
+    string  type;           // Pet type
+    int     age;            // Pet age
+
+    CostInfo cost;          // A PetInfo structure has a CostInfo structure
+                            // nested inside as one of its members
+
+    PetInfo()               // Default constructor
+    {
+        name = "unknown";
+        type = "unknown";
+        age = 0;
+        cost.food = cost.medical = cost.license = cost.misc = 0.00;
+    }
+};
+
+int main()
+{
+    // Define a PetInfo structure variable called pet
+    PetInfo pet;
+
+    // Assign values to the pet member variables.
+    // Notice that cost.misc is not assigned a value,
+    // so it remains 0, as set by the constructor.
+    pet.name = "Sassy";
+    pet.type = "cat";
+    pet.age = 5;
+    pet.cost.food = 300.00;
+    pet.cost.medical = 200.00;
+    pet.cost.license = 7.00;
+
+    //
+}
