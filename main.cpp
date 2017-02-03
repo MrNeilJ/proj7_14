7fadae66ca17703d9915dd3e104898264b3d9723
Neil Johnson <mr.neil.j@gmail.com>
2017-02-03 00:28:21 -0800

    Added first struct to file Completed rest of the structs required Completed the rest of the main function

--- main.cpp
+++ main.cpp
@@ -45,5 +45,12 @@ int main()
     pet.cost.medical = 200.00;
     pet.cost.license = 7.00;
 
-    //
+    // Display the total annual costs for the pet
+    cout    << fixed << showpoint << setprecision(2);
+    cout    << "Annual costs for my " << pet.age << "-year-old "
+            << pet.type << " " << pet.name << " are $"
+            << (pet.cost.food + pet.cost.medical +
+                pet.cost.license + pet.cost.misc) << endl;
+
+    return 0;
 }
