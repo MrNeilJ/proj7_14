86a1cbd07bec6dcc48e9269738961c158f85c21d
Neil Johnson <mr.neil.j@gmail.com>
2017-02-03 00:21:01 -0800

    Added first struct to file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
 // This program demonstrates the use of a nested structure.
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+struct CostInfo
+{
+    double  food,           // Food costs
+            medical,        // Medical costs
+            license,        // License fee
+            misc;           // Miscellaneious costs
+};
